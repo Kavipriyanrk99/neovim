21c7327cd70281ccca088bced1ae18219d22d6d0
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-11-10 22:17:28 +0530

    capabilities added to lsp.lua

--- lua/kavipriyanrk99/plugins/lsp.lua
+++ lua/kavipriyanrk99/plugins/lsp.lua
@@ -14,8 +14,11 @@ return {
             },
         }
 
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
         local lspconfig = require("lspconfig")
         lspconfig.lua_ls.setup {
+            capabilities = capabilities,
             settings = {
                 Lua = {
                   diagnostics = {
@@ -32,6 +35,7 @@ return {
         }
 
         lspconfig.jdtls.setup{
+            capabilities = capabilities,
             cmd = { 'jdtls' },
         }
     end
