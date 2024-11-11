2da4e82e9c9637f1b98770689f39427c75fa6453
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-11-11 11:14:53 +0530

    path completion added

--- lua/kavipriyanrk99/plugins/lsp.lua
+++ lua/kavipriyanrk99/plugins/lsp.lua
@@ -1,42 +1,42 @@
 return {
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
     dependencies = {
-        { 'williamboman/mason.nvim', config = true },
-        'williamboman/mason-lspconfig.nvim',
+        { "williamboman/mason.nvim", config = true },
+        "williamboman/mason-lspconfig.nvim",
     },
 
     config = function()
         require("mason").setup()
-        require("mason-lspconfig").setup {
+        require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
                 "jdtls",
             },
-        }
+        })
 
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup {
+        lspconfig.lua_ls.setup({
             capabilities = capabilities,
             settings = {
                 Lua = {
-                  diagnostics = {
-                    globals = { "vim" },
-                  },
-                  workspace = {
-                    library = {
-                      [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-                      [vim.fn.stdpath "config" .. "/lua"] = true,
+                    diagnostics = {
+                        globals = { "vim" },
                     },
-                  },
+                    workspace = {
+                        library = {
+                            [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                            [vim.fn.stdpath("config") .. "/lua"] = true,
+                        },
+                    },
                 },
-            }
-        }
+            },
+        })
 
-        lspconfig.jdtls.setup{
+        lspconfig.jdtls.setup({
             capabilities = capabilities,
-            cmd = { 'jdtls' },
-        }
-    end
+            cmd = { "jdtls" },
+        })
+    end,
 }
